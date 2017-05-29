70b6ee1e1545edf2ce608eb7115ad3d8e6b06779
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-05-29 06:57:42 +0200

    Serve a templated landing page

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mustacheExpress = require('mustache-express');
 const app = express();
 
 const port = 3000;
 
+app.engine('mustache', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('views', './views');
+
 app.use(express.static('resources'));
+
+app.post('/start', function(req, res){
+    res.render('landing', { id: 37 });
+});
 
 app.get('/hello', function(req, res){
     res.send('Hello, World!');
