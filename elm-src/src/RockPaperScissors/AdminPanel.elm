7d6a1cb59b2991bcbf5c1dc387a0cfbc0c7b5923
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-06-05 10:46:13 +0200

    Create an AdminPanel

---
+++ elm-src/src/RockPaperScissors/AdminPanel.elm
@@ -0,0 +1,123 @@
+module RockPaperScissors.AdminPanel exposing (main)
+
+
+import Html
+import Html.Events as Event
+
+
+main : Program Never Model Message
+main =
+    Html.program
+    {
+      init = init
+    , update = update
+    , view = view
+    , subscriptions = subscriptions
+    }
+
+
+init : (Model, Cmd Message)
+init = (emptyModel "start", Cmd.none)
+
+
+type alias Model =
+    {
+      history: List GameInfo
+    , current: GameInfo
+    }
+
+
+type alias GameInfo =
+    {
+      gameId: GameId
+    , registeredPlayers: Int
+    , choicesMade: Int
+    }
+
+
+type alias GameId = String
+
+
+type Message =
+      DoNothing
+    | PlayerRegistered GameId
+    | ChoiceMade GameId
+    | Resolve
+    | NextGame GameId
+
+
+emptyModel : GameId -> Model
+emptyModel gameId =
+    { history = [], current = emptyGameInfo gameId }
+
+
+emptyGameInfo : GameId -> GameInfo
+emptyGameInfo gameId =
+    { gameId = gameId, registeredPlayers = 0, choicesMade = 0 }
+
+
+update : Message -> Model -> (Model, Cmd Message)
+update message model =
+    case message of
+        DoNothing -> (model, Cmd.none)
+
+        PlayerRegistered gameId ->
+           if gameId == model.current.gameId then
+               ({ model | current = registerPlayer model.current }, Cmd.none)
+           else
+               (model, Cmd.none)
+
+        ChoiceMade gameId ->
+            if gameId == model.current.gameId then
+                ({ model | current = registerChoice model.current }, Cmd.none)
+            else
+                (model, Cmd.none)
+
+        Resolve ->
+            (model, Cmd.none)
+
+        NextGame gameId ->
+            let
+                history = model.current :: model.history
+
+                current = emptyGameInfo gameId
+            in
+                ({ model | current = current, history = history }, Cmd.none)
+
+
+registerPlayer : GameInfo -> GameInfo
+registerPlayer gameInfo =
+    { gameInfo | registeredPlayers = gameInfo.registeredPlayers + 1 }
+
+
+registerChoice : GameInfo -> GameInfo
+registerChoice gameInfo =
+    { gameInfo | choicesMade = gameInfo.choicesMade + 1 }
+
+
+view : Model -> Html.Html Message
+view model =
+    let
+        currentView = [viewGameInfo model.current]
+
+        historyView = List.map viewGameInfo model.history
+    in
+        Html.div
+            []
+            (List.concat [currentView, historyView])
+
+
+viewGameInfo : GameInfo -> Html.Html Message
+viewGameInfo gameInfo =
+    Html.div
+        []
+        [
+          Html.span [] [ Html.text ("id: " ++ (toString gameInfo.gameId)) ]
+        , Html.span [] [ Html.text ("registered players: " ++ (toString gameInfo.registeredPlayers)) ]
+        , Html.span [] [ Html.text ("choices made: " ++ (toString gameInfo.choicesMade)) ]
+        , Html.button [ Event.onClick Resolve ] [ Html.text "resolve" ]
+        ]
+
+
+subscriptions : Model -> Sub Message
+subscriptions _ = Sub.none
